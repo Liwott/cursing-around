31153af91ee48f27e63f9fef4c5baaaa0ba15d31
David <david.defilippi@hotmail.be>
2022-02-10 00:48:22 +0100

    clean up

--- spec/cursing-around-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CursingAroundView from '../lib/cursing-around-view';
-
-describe('CursingAroundView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
