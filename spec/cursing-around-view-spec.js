f9f15c6680a87120251023595bc53aa1f77912c7
David <david.defilippi@hotmail.be>
2022-02-09 23:35:16 +0100

    package-generator:generate-package

---
+++ spec/cursing-around-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CursingAroundView from '../lib/cursing-around-view';
+
+describe('CursingAroundView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
